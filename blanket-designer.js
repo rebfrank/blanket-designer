bd360714ed5d957c9cd6bc110b60f026c70c2ee7
Rebecca Frank <rebeccaffrank@gmail.com>
2019-11-17 14:47:53 -0700

    Dropdown for several color schemes

--- blanket-designer.js
+++ blanket-designer.js
@@ -1,4 +1,8 @@
-scheme1 = [ "#555358", "#C6CA53", "#678C69", "#C9DCB3", "#F1F5F2" ]
+const colorSchemes = {
+    "Scheme 1": [ "#555358", "#C6CA53", "#678C69", "#C9DCB3", "#F1F5F2" ],
+    "Scheme 2": [ "#2A4F2B", "#B098A4", "#678C69", "#DBD9DB", "#E5EBEA" ],
+    "Scheme 3": [ "#CFF09E", "#A8DBA8", "#678C69", "#72B5B5", "#0B486B" ]
+};
 
 function makeQuiltArray(colorScheme) {
     var quiltArray = [];
@@ -26,4 +30,17 @@ function makeTableHTML(colorArray) {
     return tableHTML;
 }
 
-document.getElementById("quilt").innerHTML = makeTableHTML(makeQuiltArray(scheme1));
+function makeDropdownList(list) {
+    return list.map(item => `<option value="${item}">${item}</option>`).join("");
+}
+
+function drawBlanket(schemeName) {
+    document.getElementById("quilt").innerHTML = makeTableHTML(makeQuiltArray(colorSchemes[schemeName]));
+}
+
+const schemeSelectorElement = document.getElementById("schemeSelector");
+schemeSelectorElement.innerHTML = makeDropdownList(Object.keys(colorSchemes));
+schemeSelectorElement.addEventListener('change', (event) => {
+    drawBlanket(event.target.value);
+});
+drawBlanket("Scheme 1");
