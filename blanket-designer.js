5f0e6ef9c91036b65e69e076677741cbd68bbc55
Rebecca Frank <rebeccaffrank@gmail.com>
2019-11-17 16:02:39 -0700

    Improve styling on scheme selector

--- blanket-designer.js
+++ blanket-designer.js
@@ -1,7 +1,12 @@
 const colorSchemes = {
     "Scheme 1": [ "#555358", "#C6CA53", "#678C69", "#C9DCB3", "#F1F5F2" ],
     "Scheme 2": [ "#2A4F2B", "#B098A4", "#678C69", "#DBD9DB", "#E5EBEA" ],
-    "Scheme 3": [ "#CFF09E", "#A8DBA8", "#678C69", "#72B5B5", "#0B486B" ]
+    "Scheme 3": [ "#CFF09E", "#A8DBA8", "#678C69", "#72B5B5", "#0B486B" ],
+    "Scheme 4": [ "#FE4365", "#FC9D9A", "#678C69", "#C8C8A9", "#F9CDAD" ],
+    "Scheme 5": [ "#FFED90", "#A8D46F", "#678C69", "#3C3251", "#341139" ],
+    "Scheme 6": [ "#D4E6B5", "#AFC97E", "#678C69", "#E2D686", "#FFDF64" ],
+    "Scheme 7": [ "#F4FDD9", "#E4FFE1", "#678C69", "#FFE8C2", "#FFB06B" ],
+    "Scheme 8": [ "#C5E063", "#E3E9C2", "#678C69", "#F9FBB2", "#A5ABAF" ],
 };
 
 function makeQuiltArray(colorScheme) {
